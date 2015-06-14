73c998f99f58506b14b9f34f7d27ebc8c0d9cfde
elena <elena.15@hotmail.it>
2015-06-14 15:46:28 +0100

    added navigation and styled

--- spec/features/homepage_feature_spec.rb
+++ spec/features/homepage_feature_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 feature 'homepage' do
   scenario 'should display the links to other pages' do
     visit '/homepage'
-    expect(page).to have_link 'Services'
+    expect(page).to have_link 'Portfolio'
     expect(page).to have_link 'Packages & Prices'
-    expect(page).to have_link 'Gallery'
+    expect(page).to have_link 'About'
     expect(page).to have_link 'Contact'
   end
 end
