dbcede10bf9a498313fcf6396605c95a8698deb2
elenagarrone <elena.15@hotmail.it>
2016-10-22 20:03:35 +0100

    add new dropdown functionality, remove image from p&p, change some content

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,3 +31,23 @@ $("#nav-toggle").on( "click", function() {
   $('.nav-links-list').toggleClass( "active" );
   $('.header').toggleClass( "active" );
 });
+
+(function(){
+  var activeClass = 'is_active'
+  var dropdownBtnClass = '.dropdown_btn';
+  var $instances = $(dropdownBtnClass);
+  $instances.each(function(i, el){
+    var index = i;
+    var $el = $(el);
+    var text = $el.text();
+    var $listItem = $($el.closest('li'));
+    $listItem.attr({'id': 'dropdown_menu_' + i, 'class': 'dropdown_menu'});
+    $listItem.find('a').remove();
+    $listItem.prepend('<button role="button" id="ddbutton_' + i +'" class="dropdown_btn">' + text + '</button>');
+    $listItem.on('click', dropdownBtnClass, function(){
+      var $this = $(this);
+      $this.closest('li').toggleClass(activeClass);
+    });
+  });
+
+})();
